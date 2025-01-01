012f9ae2df719aeb74be5c05005d7b11795fd28b
Chu Huynh <huynhminhchu1999@gmail.com>
2025-01-01 02:14:13 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "antd";
+
+export default function Home(){
+  return (
+    <div className="p-2 text-red-500">
+      Hello
+      <div>
+        <Button type="primary">
+          hello
+        </Button>
+      </div>
+    </div>
+
+  )
+}
